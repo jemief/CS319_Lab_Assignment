7b62d020f4b0cd824386c9a9debf9f3f225314b7
jemief <61112134+jemief@users.noreply.github.com>
2020-04-07 12:33:07 +0300

    Corrected to Descented order

--- src/ArrayOrganizer.java
+++ src/ArrayOrganizer.java
@@ -1,19 +1,25 @@
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
-
 public class ArrayOrganizer {
-
     public ArrayList<Integer> concatAndSort(ArrayList<Integer> list1, ArrayList<Integer> list2, ArrayList<Integer> list3) {
         ArrayList<Integer> result = new ArrayList<>();
         result.addAll(list1);
         result.addAll(list2);
-//<<<<<<< HEAD
-        Collections.sort(result, Collections.reverseOrder());
-//=======
         result.addAll(list3);
-        Collections.sort(result);
-//>>>>>>> origin/feature-branch
+        for(int i = 0; i < result.size() -1 ; i++)
+        {
+            for(int j = 0; j < result.size()-i-1;j++)
+            {
+                if(result.get(j) < result.get(j+1)) 
+                {
+                    int temp = result.get(j);
+                    result.set(j, result.get(j+1));
+                    result.set(j+1,temp);
+                }
+            }
+        }
         return result;
     }
-
 }
